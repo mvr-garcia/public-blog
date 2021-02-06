020f5ec6f4df2eff37ace36f137a4c4f5f65b33f
mvr-garcia <vinicius-garcia13@hotmail.com>
2021-02-06 12:57:01 -0300

    Configure error page

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_heroku
 
@@ -64,7 +65,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
